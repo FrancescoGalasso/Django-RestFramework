cff9205848105813ae99e69578ff15afd094cf53
FrancescoGalasso <francescogalasso@alfadispenser.com>
2018-02-12 14:56:34 +0100

    Django REST Framework - end tut 5

--- snippets/views.py
+++ snippets/views.py
@@ -2,12 +2,26 @@
 from __future__ import unicode_literals
 
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import UserSerializer,SnippetSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -32,3 +46,10 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
